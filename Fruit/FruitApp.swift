897c6d91f892e8f4ae1bca3e9ece65e7ad33bb8c
LilDankPrince <91747357+LilDankPrince@users.noreply.github.com>
2023-03-06 12:44:45 +0200

    Initial Commit

---
+++ Fruit/FruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitApp.swift
+//  Fruit
+//
+//  Created by Dominique Parkin on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct FruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
